786d9dfa9d1fc3c2a418f9d79fd44889c753f49c
cristina <cristina.ilea2@yahoo.com>
2023-06-28 20:40:51 +0300

    curs 28 iunie - Maven

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public double sum(double x, double y){
+        return x+y;
+    }
+
+    public double multiply(double x, double y){
+        return x*y;
+    }
+}
---
+++ src/main/java/IsPositive.java
@@ -0,0 +1,5 @@
+public class IsPositive {
+    public boolean isPositive(double number){
+        return number>=0;
+    }
+}
